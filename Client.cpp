4a0cad53382f41c0ca5b21e7ed379d29203aba74
Mikhail_Shell <theoutstandingme@gmail.com>
2025-09-15 18:09:00 +0700

    Created a client

---
+++ Client.cpp
@@ -0,0 +1,84 @@
+﻿#include <iostream>
+#include <fstream>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#pragma comment(lib, "Ws2_32.lib")
+
+#define BUFFER_SIZE 1024
+#define TIMEOUT 2000
+
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+    setlocale(LC_ALL, "Russian");
+
+    WSADATA wsadata;
+    if (WSAStartup(MAKEWORD(2, 2), &wsadata) != 0) {
+        cout << "Не удалось открыть сокет" << endl;
+        return -1;
+    }
+
+    SOCKET clientSocket = socket(AF_INET, SOCK_DGRAM, 0);
+    if (clientSocket == -1) {
+        cout << "Не удалось открыть сокет" << endl;
+        WSACleanup();
+        return -1;
+    }
+
+    sockaddr_in clientAddress;
+    clientAddress.sin_family = AF_INET;
+    clientAddress.sin_port = 0;
+    clientAddress.sin_addr.s_addr = INADDR_ANY;
+    if (bind(clientSocket, (sockaddr*)&clientAddress, sizeof(sockaddr)) == -1) {
+        cout << "Не удалось связать сокет" << endl;
+        WSACleanup();
+        return -2;
+    }
+
+    sockaddr_in serverAddress;
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_port = htons(atoi(argv[2]));
+    InetPtonA(AF_INET, argv[1], &serverAddress.sin_addr.s_addr);
+    int serverAddressLength = sizeof(sockaddr_in);
+
+    int timeout = TIMEOUT;
+    setsockopt(clientSocket, SOL_SOCKET, SO_RCVTIMEO, (char*)&timeout, sizeof(int));
+
+    ifstream file("some.txt", ios::in);
+    char buffer[BUFFER_SIZE];
+    
+    while (true) {
+        file.read(buffer, BUFFER_SIZE - 4);
+        int bytesRead = file.gcount();
+        if (bytesRead <= 0) {
+            break;
+        }
+        memmove(buffer + 4, buffer, bytesRead);
+        cout.write(buffer + 4, bytesRead);
+        cout << endl;
+        int clientIndex = atoi(argv[3]);
+        memcpy(buffer, &clientIndex, sizeof(int));
+        int bytesSent, receivedBytes, attempts = 0;
+        do {
+            attempts += 1;
+            if (attempts > 1) {
+                cout << "Попытка отправки № " << attempts << endl;
+            }
+            bytesSent = sendto(clientSocket, buffer, bytesRead + 4, 0, (sockaddr*)&serverAddress, serverAddressLength);
+            receivedBytes = recvfrom(clientSocket, buffer, 3, 0, (sockaddr*)&serverAddress, &serverAddressLength);
+            if (receivedBytes != SOCKET_ERROR) {
+                cout << "Получено подтверждение" << endl;
+            } else if (attempts == 5) {
+                cout << "Пропуск" << endl;
+            }
+        } while (receivedBytes == SOCKET_ERROR && attempts <= 5);
+    }
+
+    closesocket(clientSocket);
+    WSACleanup();
+
+    return 0;
+}
